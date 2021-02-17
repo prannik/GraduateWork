38a00accf76a508c83cbcfd0b13045f777fa1405
Maksim <prannik.m@gmail.com>
2021-02-18 02:49:26 +0300

    second

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(k-cyql8w(!l4%%ro+pqh3bl70!-q@idgn@&aq0^s6iv#a1i8='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'server-domain_or_IP' ]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'graduateworkdb',
+        'USER': 'maksim',
+        'PASSWORD': 'qwerty111',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
