b61d5980ad0f645f89000d4f609857364a8bdc9a
Maksim <prannik.m@gmail.com>
2021-03-22 17:20:41 +0300

    Update load images

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
