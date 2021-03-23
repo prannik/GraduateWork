d5872eec7b9cec2dc919d294e47cba68abda4a02
prannik.m <63631198+prannik@users.noreply.github.com>
2021-03-23 15:22:50 +0300

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -4,12 +4,12 @@ from shop.models import Product
 
 
 class Order (models.Model):
-    first_name = models.CharField(max_length=60, verbose_name='Фамилия', blank=True, null=True)
-    last_name = models.CharField(max_length=60, verbose_name='Имя', blank=True, null=True)
-    email = models.EmailField(verbose_name='Email', blank=True, null=True)
-    address = models.CharField(max_length=150, verbose_name='Адрес', blank=True, null=True)
-    postal_code = models.CharField(max_length=30, verbose_name='Индекс', blank=True, null=True)
-    city = models.CharField(max_length=100, verbose_name='Город', blank=True, null=True)
+    first_name = models.CharField(max_length=60, verbose_name='Фамилия', blank=True)
+    last_name = models.CharField(max_length=60, verbose_name='Имя', blank=True)
+    email = models.EmailField(verbose_name='Email', blank=True)
+    address = models.CharField(max_length=150, verbose_name='Адрес', blank=True)
+    postal_code = models.CharField(max_length=30, verbose_name='Индекс', blank=True)
+    city = models.CharField(max_length=100, verbose_name='Город', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False) 
