43dd32adb017b7ecb32b721326437ed9a256e049
Maksim <prannik.m@gmail.com>
2021-04-29 00:12:06 +0300

    s3 is work

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,14 +118,10 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#
-# MEDIA_URL = 'static/img/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/')
-#
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
